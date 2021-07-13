cb14ffb74dc95b57f0b495acf493fe54dc3adb45
Alae Ez-zaouine <aez-zaou@student.1337.ma>
2021-07-13 21:26:57 +0100

    creating philosophers and their forks

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,31 +6,38 @@
 /*   By: aez-zaou <aez-zaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 18:44:52 by aez-zaou          #+#    #+#             */
-/*   Updated: 2021/07/12 20:50:58 by aez-zaou         ###   ########.fr       */
+/*   Updated: 2021/07/13 21:25:17 by aez-zaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+int get_index(t_args *args)
+{
+    int i;
+
+    i = 0;
+    while (args->index[i] == 'a')
+        i++;
+    args->index[i] = 'a';
+    return (i);
+}
 
 void *routine(void *args2)
 {
     t_args *args;
-    int *id;
-    int i = 0;
+    int id;
+    int i;
     
-    // pthread_mutex_lock(&g_mutex);
     args = (t_args *)args2;
-    while (args->index[i] == 'a')
-        i++;
-    args->index[i] = 'a';
-    id = malloc(sizeof(int));
-    *id = i + 1;
-    printf("philo num %d is alive\n", *id);
-    // sleep(1);
-    // printf("Im alive again\n");
-    // pthread_mutex_unlock(&g_mutex);
-    
-    return (id);
+    id = get_index(args);
+    pthread_mutex_lock(&args->forks[id]);
+    printf("philo num %d is alive\n", id);
+
+
+
+
+    pthread_mutex_unlock(&args->forks[id]);
+    return (NULL);
 }
 
 int main()
@@ -41,8 +48,10 @@ int main()
     void *ret_val;
     t_args args;
     
+    args.test = 0;
+    args.philo_num = 10;
     args.index = (char *)malloc(number_of_philosophers * sizeof(char));
-    pthread_mutex_init(&g_mutex, NULL);
+    init_forks(&args);
     while (i < number_of_philosophers)
     {
         args.index[i] = 'a';
@@ -52,26 +61,42 @@ int main()
     // int time_to_eat;
     // int time_to_sleep;
     i = 0;
-    pthread_t *th;
 
-    th = (pthread_t *)malloc(number_of_philosophers * sizeof(pthread_t));
+    create_thread(&args);
+   
 
+    
+    return (0);
+}
+
+void    init_forks(t_args *args)
+{
+    int i;
+
+    i = 0;
+    args->forks = (pthread_mutex_t *)malloc(args->philo_num * sizeof(pthread_mutex_t));
+    while (i < args->philo_num)
+    {
+        pthread_mutex_init(&args->forks[i] , NULL);
+
+    }
+}
+
+void create_thread(t_args *args)
+{
+    args->philos = (pthread_t *)malloc(args->philo_num * sizeof(pthread_t));
     while (i < 10)
     {
         args.index[i] = 'f';
-        if (pthread_create(th + i, NULL, &routine, (void*)&args) != 0)
+        if (pthread_create(args->philos + i, NULL, &routine, (void*)args) != 0)
 			return (1);
-        // args.index[i] = 'a';
         i++;
     }
     i = 0;
     while (i < 10)
     {
-        pthread_join(th[i], &ret_val);
-        philo_num = (int *)ret_val;
-        printf("philo num %d is done\n", *philo_num);
-        free(philo_num);
+        pthread_join(args->philos[i], NULL);
 		i++;
     }
-    return (0);
+    printf("test = %d\n", args.test);
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: aez-zaou <aez-zaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 18:44:54 by aez-zaou          #+#    #+#             */
-/*   Updated: 2021/07/12 20:10:21 by aez-zaou         ###   ########.fr       */
+/*   Updated: 2021/07/13 20:39:51 by aez-zaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@
 #include <unistd.h>
 
 typedef struct s_args{
+    int philo_num;
+    int *time_eat;
+    int *time_die;
     char *index;
-    pthread_mutex_t mutex;
+    pthread_t *philos;
+    pthread_mutex_t *forks;
+    int test;
 }t_args;
 
 pthread_mutex_t g_mutex;
